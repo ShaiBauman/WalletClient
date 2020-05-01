7319fe29bae559cc864c9423a7c0e7eec2b13baf
Shai Bauman <Shai.bauman@gmail.com>
2020-05-01 18:39:29 +0300

    add main screens and build navigation

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import SettingsScreen from "./src/Screens/SettingsScreen";
+import PurchaseScreen from "./src/Screens/PurchaseScreen";
+import SignUpScreen from "./src/Screens/SignUpScreen";
+import SignInScreen from "./src/Screens/SignInScreen";
+import IndexWelletScreen from "./src/Screens/IndexWelletScreen";
+import IndexFriendScreen from "./src/Screens/IndexFriendScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Thank You!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignUpScreen,
+    Signin: SignInScreen
+  }),
+  welletMainFlow: createStackNavigator({
+    indexWellet: IndexWelletScreen,
+    makePurchase: PurchaseScreen,
+    Settings: SettingsScreen,
+  }),
+  friendMainFlow: createStackNavigator({
+    indexFriend: IndexFriendScreen,
+    Settings: SettingsScreen,
+  })
 });
+
+const App = createAppContainer(switchNavigator);
+
+export default ()=>{
+  return(
+      <AuthProvider>
+        <App/>
+      </AuthProvider>
+  );
+};
+
+
---
+++ src/Screens/SignUpScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native'
+
+const SignUpScreen = ()=>{
+    return(
+        <View>
+            <Text style={{fontSize:48}}>Sign Up Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignUpScreen;
